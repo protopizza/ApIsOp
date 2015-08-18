7da5ac1707cb819f9b45b2442cf6bf835bf4d6ed
so <sosoi@ucdavis.edu>
2015-08-18 03:10:14 -0700

    adding champion icons to dropdown menus

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ $('.ui.dropdown').dropdown({
 });
 
 $.getJSON('data/static/champions.json', function(data){
+    var champsObj = data.data;
+    console.log(champsObj);
     $.each(data.data, function(key, val){
-        champs.push(val.name);
+        champs.push(key);
     })
-    champs.sort();
 
+    champs.sort();
+    console.log(champs);
     for( var i in champs ){
-        $(".dropdown .menu").append('<div class="item">' + champs[i] + '</div>');
+        $(".dropdown .menu").append(
+            '<div class="item" data-value="' + champs[i] + '"><img class="ui avatar image" src="assets/' + champs[i] + '.png">' + champsObj[champs[i]].name + '</div>');
     }
 })
 
