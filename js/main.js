1296e37e9f4335484e2880852a45ee9b076c6c2c
so <sosoi@ucdavis.edu>
2015-08-18 02:22:07 -0700

    adding max champ select count

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+var champs = [];
+
 $('.ui.dropdown').dropdown({
-    apiSettings: {
-        url: 'champions.json'
-    }
+    maxSelections: 5
 });
+
+$.getJSON('champions.json', function(data){
+    $.each(data.data, function(key, val){
+        champs.push(val.name);
+    })
+    champs.sort();
+
+    for( var i in champs ){
+        $(".dropdown .menu").append('<div class="item">' + champs[i] + '</div>');
+    }
+})
+
+$('#champselect-A').dropdown('refresh');
+$('#champselect-B').dropdown('refresh');
