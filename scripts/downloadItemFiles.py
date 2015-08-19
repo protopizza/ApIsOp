15aeb7e85ceeb2bfbc3620e636ed4938e7db2311
so <sosoi@ucdavis.edu>
2015-08-19 01:22:43 -0700

    replacing item files to be from 514

--- scripts/downloadItemFiles.py
+++ scripts/downloadItemFiles.py
@@ -4,11 +4,6 @@ import urllib
 def main():
 	with open('../data/static/items514.json', 'r') as fp:
 		item_list = json.load(fp)
-
-	total_count = len(item_list["data"])
-
 	for key in item_list["data"]:
-		urllib.urlretrieve("http://ddragon.leagueoflegends.com/cdn/5.11.1/img/item/"+key+".png", key+".jpg")
-	# print(json.dumps(item_list["data"], sort_keys=True, indent=4, separators=(',',':')))
-
+		urllib.urlretrieve("http://ddragon.leagueoflegends.com/cdn/5.14.1/img/item/"+key+".png", key+".jpg")
 main()
