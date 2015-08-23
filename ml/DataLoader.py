edafb4f2d8fc68f67934586bf014dafa3d3bd009
protopizza <aznchipmunk@gmail.com>
2015-08-23 01:42:57 -0700

    DataLoader module to read match files and return in a generator

---
+++ ml/DataLoader.py
@@ -0,0 +1,117 @@
+import json
+
+class DataLoader(object):
+
+    NORMAL_INPUT_PATH_BASE = "../MATCH_DATA/{patch}/NORMAL_5X5/{region}/{filepatch}-normal-{fileregion}-{fileindex}.json"
+    RANKED_INPUT_PATH_BASE = "../MATCH_DATA/{patch}/RANKED_SOLO/{region}/{tier}/{filepatch}-ranked-{fileregion}-{filetier}-{fileindex}.json"
+
+    REGIONS = ["NA"] #["BR", "EUNE", "EUW", "KR", "LAN", "LAS", "NA", "OCE", "RU", "TR"]
+    PATCHES = ["5.11", "5.14"]
+    QUEUETYPES = {
+        "NORMAL_5X5":"normal",
+        "RANKED_SOLO":"ranked"
+        }
+    RANKED_TIERS = {
+        "NORMAL_5X5":[
+            "NO_RANK"
+        ],
+        "RANKED_SOLO":[
+            "BRONZE", "SILVER", "GOLD", "PLATINUM", "DIAMOND+"
+        ]
+    }
+
+    DIAMOND_PLUS = ["DIAMOND", "MASTER", "CHALLENGER"]
+    MAX_ITEMS = 6
+
+    def __init__(self):
+        pass
+
+
+    def getMatch(self):
+        for region in DataLoader.REGIONS:
+            for patch in DataLoader.PATCHES:
+                for queueType in DataLoader.QUEUETYPES:
+                    for tier in DataLoader.RANKED_TIERS[queueType]:
+                        fileindex = 0
+                        try:
+                            while (True):
+                                BASE_PATH = ""
+                                if DataLoader.QUEUETYPES[queueType] == "ranked":
+                                    BASE_PATH = DataLoader.RANKED_INPUT_PATH_BASE.format(patch=patch, region=region, filepatch=patch.replace(".", ""), tier=tier, fileregion=region, filetier=tier.lower(), fileindex=fileindex)
+                                else:
+                                    BASE_PATH = DataLoader.NORMAL_INPUT_PATH_BASE.format(patch=patch, region=region, filepatch=patch.replace(".", ""), fileregion=region, fileindex=fileindex)
+                                print "reading from {}...".format(BASE_PATH)
+                                with open(BASE_PATH, 'r') as fp:
+                                    matches_data = json.load(fp)
+                                    for sequence in matches_data:
+                                        matches_data[sequence]["matchTier"] = tier
+                                        yield matches_data[sequence]
+                                fileindex += 1
+                        except IOError:
+                            print "done reading tier:{} in queueType:{} in patch:{} in region:{}".format(tier, queueType, patch, region)
+
+
+    def filterMatchFields(self, match):
+        participants = match["participants"]
+        match["matchVersion"] = '.'.join(match["matchVersion"].split('.')[:2])
+        desired_keys = [
+            "matchId",
+            "matchTier",
+            "matchType",
+            "matchVersion",
+            "queueType",
+            "region"
+        ]
+        match = { key: match[key] for key in desired_keys}
+
+        stats = []
+        for player in participants:
+            stats.append(player["stats"])
+            player["stats"]["championId"] = player["championId"]
+            if player["teamId"] == 100:
+                player["stats"]["teamId"] = "A"
+            else:
+                player["stats"]["teamId"] = "B"
+
+        teamA = []
+        teamB = []
+
+        for player in stats:
+            if player["teamId"] == "A":
+                teamA.append(player)
+            else:
+                teamB.append(player)
+
+        if teamA[0]["winner"]:
+            match['winnerTeamA'] = True
+        else:
+            match['winnerTeamA'] = False
+
+        stats_keys = [
+            "championId",
+            "teamId",
+            "items"
+        ]
+
+        for player in range(len(teamA)):
+            items = []
+            for idx in range(DataLoader.MAX_ITEMS):
+                if teamA[player]["item{}".format(idx)] == 0:
+                    continue
+                items.append(teamA[player]["item{}".format(idx)])
+            teamA[player]["items"] = items
+            teamA[player] = { key: teamA[player][key] for key in stats_keys}
+
+        for player in range(len(teamB)):
+            items = []
+            for idx in range(DataLoader.MAX_ITEMS):
+                if teamB[player]["item{}".format(idx)] == 0:
+                    continue
+                items.append(teamB[player]["item{}".format(idx)])
+            teamB[player]["items"] = items
+            teamB[player] = { key: teamB[player][key] for key in stats_keys}
+
+        match['teamA'] = teamA
+        match['teamB'] = teamB
+
+        return match
--- ml/main.py
+++ ml/main.py
@@ -1,11 +1,21 @@
 from sklearn import datasets
+from DataLoader import DataLoader
+import json
+import math
+import os
+import sys
 
-iris = datasets.load_iris()
 
-digits = datasets.load_digits()
 
-print digits.data
 
-print digits.target
+def main():
 
-print digits.images[0]
+    dl = DataLoader()
+    matchGenerator = dl.getMatch()
+
+    print json.dumps(dl.filterMatchFields(matchGenerator.next()), sort_keys=True, indent=4)
+    print json.dumps(dl.filterMatchFields(matchGenerator.next()), sort_keys=True, indent=4)
+
+
+if __name__ == "__main__":
+    main()
