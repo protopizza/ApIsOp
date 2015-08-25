b211ad0e3ee65a2e3edbf79003b4f17dba232499
so <sosoi@ucdavis.edu>
2015-08-25 01:24:54 -0700

    sorted graph with items as ticks

--- js/item-visualization.js
+++ js/item-visualization.js
@@ -1,6 +1,6 @@
 function initChart(){
   // define margins, width, height of chart container
-  var margin = {top: 20, right: 100, bottom: 30, left: 50},
+  var margin = {top: 20, right: 100, bottom: 50, left: 50},
       width = 840 - margin.left - margin.right,
       height = 300 - margin.top - margin.bottom;
 
@@ -12,16 +12,17 @@ function initChart(){
   var y = d3.scale.linear()
       .rangeRound([height, 0]);
 
-    // define colors to be used per item
+  // define colors to be used per item
   var color = d3.scale.ordinal()
-      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+      .range(["#28B8C8", "#CB5C5C"]);
 
-    // define x-axis settings
+  // define x-axis settings
   var xAxis = d3.svg.axis()
       .scale(x)
+      .ticks(0)
       .orient("bottom");
 
-    // define y-axis settings
+  // define y-axis settings
   var yAxis = d3.svg.axis()
       .scale(y)
       .orient("left")
@@ -51,7 +52,7 @@ function initChart(){
       d.winRate.forEach(function(d) { d.y0 /= y0; d.y1 /= y0; });
     });
 
-    // data.sort(function(a, b) { return b.winRate[0].y1 - a.winRate[0].y1; });
+    data.sort(function(a, b) { return b.winRate[0].y1 - a.winRate[0].y1; });
 
     // define domain values for x-axis
   	x.domain(data.map(function(d) {
@@ -61,33 +62,46 @@ function initChart(){
 
     // append axes
     svg.append("g")
+      .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .style({ 'fill': 'rgba(0,0,0,0.65)'})
+      .style({ 'fill': 'rgba(0,0,0,0)'})
       .call(xAxis);
 
-      svg.append("g")
-        .attr("class", "y axis")
-        .style({ 'fill': 'rgba(0,0,0,0.65)'})
-        .call(yAxis);
+    svg.select("x.axis").selectAll("text").remove();
+    var ticks = svg.select(".axis").selectAll(".tick").data(data)
+                  .append("svg:image")
+                  .attr("xlink:href", function (d) { return 'assets/items/514/' + d['ITEM_ID'] + '.jpg'; })
+                  .attr("width", 37)
+                  .attr("height", 37)
+                  .attr('x', -20)
+                  .attr('y', 5);
 
-      // define and append percentage values for win rate per item
-      var item = svg.selectAll(".item").data(data)
-        .enter().append("g")
-          .attr("class", "item")
-          .attr("transform", function(d) { return "translate(" + x(items[d['ITEM_ID']].name) + ",0)"; });
+    svg.selectAll('.tick line')
+     .attr({'y2': 0});
 
-      item.selectAll("rect")
-            .data(function(d) { return d.winRate; })
-          .enter().append("rect")
-            .attr("width", x.rangeBand())
-            .attr("y", function(d) { return y(d.y1); })
-            .attr("height", function(d) { return y(d.y0) - y(d.y1); })
-            .style("fill", function(d) { return color(d.name); });
+    svg.append("g")
+      .attr("class", "y axis")
+      .style({ 'fill': 'rgba(0,0,0,0.65)'})
+      .call(yAxis);
 
-      svg.append("g")
-        .attr("class", "midaxis")
-        .attr("transform", "translate(0," + height/2 + ")")
-        .style({ 'stroke': 'rgba(0,0,0,0.65)', 'fill': 'none', 'stroke-width': '1px'})
-        .call(midAxis);
+    // define and append percentage values for win rate per item
+    var item = svg.selectAll(".item").data(data)
+      .enter().append("g")
+        .attr("class", "item")
+        .attr("transform", function(d) { return "translate(" + x(items[d['ITEM_ID']].name) + ",0)"; });
+
+    item.selectAll("rect")
+          .data(function(d) { return d.winRate; })
+        .enter().append("rect")
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.y1); })
+          .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+          .style("fill", function(d) { return color(d.name); });
+
+    svg.append("g")
+      .attr("class", "midaxis")
+      .attr("transform", "translate(0," + height/2 + ")")
+      .style({ 'stroke': 'rgba(0,0,0,0.65)', 'fill': 'none', 'stroke-width': '1px'})
+      .call(midAxis);
   });
 }
